31268e1f538386ec1c209968379fce1551d4d77d
Annie Miller <anniemiller@berkeley.edu>
2024-06-11 10:23:48 -0400

    cleaning up whitespace.py file to make code more readable

--- whitespace.py
+++ whitespace.py
@@ -2,14 +2,12 @@ import pandas as pd
 from openai import OpenAI
 import os
 import re
+import json
+import math
+import csv
 
 data = pd.read_csv('data.csv')
 
-client = OpenAI(api_key="your_api_key_here")
-
-# const openai = new OpenAI({
-#   apiKey: process.env.OPENAI_API_KEY
-# });
 
 ideas = data.Prompt.values
 
@@ -19,77 +17,143 @@ client = OpenAI(api_key=openai_api_key)
 
 MODEL="gpt-4o"
 
-
+#call to gpt-4o for inital theme generation
 completion = client.chat.completions.create(
   model=MODEL,
   messages=[
-    {"role": "system", "content": "You are an A.I. assistant with expert skill in whitespace analysis and idea generation.  You will follow the three stages of analysis below and complete them sequentially, make sure to add an aeronautics lens as well. STAGE 1: Evaluation Against the SDGs. Analyze the question and topic provided and generate ideas that answer that question while also considering the Sustainable Devleopment Goals (SDGs). The 17 SDGs are : No poverty, Zero hunger, Good health and well-being, Quality education, Gender equality, Clean water and sanitation, Affordable and clean energy, Decent work and economic growth, Industry, innovation and infrastructure, Reduced inequalities, Sustainable cities and communities, Responsible consumption and production, Climate action, Life below water, Life on land, Peace, justice and strong institutions, partnerships for the goals. STAGE 2:Evaluation Against a DVF Framework. Now analyze the previous ideas generated and the topics and questions put in to come up with new ideas and furher revise and refine existing ideas. Now analyze those against a desirable, viable, feasible framework. STAGE 3:Projected into the Future Using Needs, Trends and Capabilities.Now analyze the previous ideas generated and the topics and questions put in to come up with new ideas and push ideas futher. Try using the framework of trends, capabilities, and needs both now and in the future. And please try to decenter AI in the idea geneartion, it will be important but not the main focus. Thank you. "},
-    {"role": "user", "content": f"Hello! Could you use these ideas {ideas} to generate new ideasand return the top 20 ideas from Stage 3 that are not present in the ideas list. Thank you"}
-    ]
+    {"role": "system", "content": "You are an A.I. assistant with expert skill in whitespace analysis and idea generation. "},
+    {"role": "user", "content": f"Hello! Could you use these ideas {ideas} to pull out 10 distinct key themes or keywords that are one word and return them in a json with a description for each without new line characters. Thank you."}
+    ],
+  response_format = { "type": "json_object" }
 )
-print("Assistant: " + completion.choices[0].message.content)
-
 
 print(completion.choices[0].message.content)
 
-# Specify the file path and name
-file_path = 'whitespace_generation_gpt4o.txt'
 
-# Open the file in write mode and write the text
-with open(file_path, 'w') as file: 
-	file.write(completion.choices[0].message.content)
+# response = json.loads(completion.choices[0].message.content)
 
-input_string = completion.choices[0].message.content
 
-# # Specify the file path and name
-# file_path = 'whitespace_generation_gpt4o.txt'
+# with open("10theme1.json", "w") as outfile:
+#   json.dump(response, outfile)
 
-# # Open the file in read mode and read the content
-# with open(file_path, 'r') as file:
-#     generated_content = file.read()
 
-# print(generated_content)
+with open('10theme1.json', 'r') as openfile:
+  json_object = json.load(openfile)
 
 
 
+# print(json_object)
+data3 = pd.DataFrame(json_object['themes'])
 
-# Extract everything after "top 20"
-if start_pos != -1:
-    temp_result = input_string[start_pos + len("Top 20"):]
-else:
-	#fix this
-    temp_result = ""
+#data3.to_csv('themes1.csv', index=False)
 
-print(temp_result)
+#round 2 ideas
 
-# stopping_character = "1."
+# ideas_2 = data3['keyword'].values
 
-# result = temp_result.split(stopping_character)[1]
+# completion2 = client.chat.completions.create(model=MODEL,messages=[{"role": "system", "content": "You are an A.I. assistant with expert skill in whitespace analysis and idea generation. "}, {"role": "user", "content": f"Hello! Could you use these ideas {ideas_2} to pull out 6 distinct key themes or related keywords for every idea there. Please return them in a json with a description for each, there should be {len(ideas_2)*6} ideas in the final ouptput with a keyword and description. Thank you."}],response_format = { "type": "json_object" })
 
-pattern = r'\n\d+\.'
 
-formatted_data = re.split(pattern, temp_result)
-formatted_data = formatted_data[1:]
-formatted_data[-1] = formatted_data[-1].splitlines()[0]
-#split_result = result.splitlines()
+# print(completion2.choices[0].message.content)
 
-#print(result)
-#append_prompts = formatted_data
-append_source = ['whitespace generated' for x in range(len(formatted_data))]
-append_maturity = ['whitespace' for x in range(len(formatted_data))]
-# append_description = []
-# append_transcript = []
+# response2 = json.loads(completion2.choices[0].message.content)
 
-append_df = pd.DataFrame({'Prompt': formatted_data, 'Source': append_source, 'Maturity': append_maturity})
+# with open("10theme2.json", "w") as outfile:
+#   json.dump(response2, outfile)
+
+
+
+# with open('10theme2.json', 'r') as openfile:
+#   json_object2 = json.load(openfile)
+
+
+# data4 = pd.DataFrame(json_object2)
+
+
+# data8 = pd.json_normalize(json_object2, sep='_')
+
+# df_combined_2 = pd.DataFrame()
+# for col in data8.columns:
+#   print(col)
+#   df_temp = pd.json_normalize(json_object2, col).assign(Category=col)
+#   df_combined_2 = pd.concat([df_combined_2, df_temp], ignore_index=True)
+
+
+
+# df_combined_2.to_csv('themes2.csv', index=False)
+
+
+#round 3 ideas
+
+# ideas_3 = df_combined_2['keyword'].values
+# output= []
+
+# batch_size = 20
+# for i in range(math.ceil(len(ideas_3)/batch_size)):
+#   ideas_3_subset = ideas_3[(i*batch_size):((i+1)*batch_size)]
+#   completion3 = client.chat.completions.create(model=MODEL,messages=[{"role": "system", "content": "You are an A.I. assistant with expert skill in whitespace analysis and idea generation. "}, {"role": "user", "content": f"Hello! Could you use these ideas {ideas_3_subset} and come up with 3 distinct key themes or related keywords for every idea there. Please return them in a json with a description for each, there should be {len(ideas_3_subset)*3} ideas in the final ouptput with a keyword and description. Thank you."}],response_format = { "type": "json_object" })
+#   print(completion3.choices[0].message.content)
+#   output += [completion3.choices[0].message.content] 
+
+
+# df_combined_3 = pd.DataFrame()
+# for i in range(len(output)):
+#   temp=json.loads(output[i])
+#   data10 = pd.json_normalize(temp, sep='_')
+#   for col in data10.columns:
+#     print(col)
+#     df_temp = pd.json_normalize(temp, col).assign(Category=col)
+#     df_combined_3 = pd.concat([df_combined_3, df_temp], ignore_index=True)
+#   print(df_combined_3)
+
+# df_combined_3.to_csv('themes3.csv', index=False)
+
+# print(df_combined_3)
+
+
+# # Round 3 Idea Generation
+# ideas_4 = df_combined_3['keyword'].values
+# output= []
+
+# batch_size = 20
+# for i in range(math.ceil(len(ideas_4)/batch_size)):
+#   ideas_4_subset = ideas_4[(i*batch_size):((i+1)*batch_size)]
+#   completion4 = client.chat.completions.create(model=MODEL,messages=[{"role": "system", "content": "You are an A.I. assistant with expert skill in whitespace analysis and idea generation. "}, {"role": "user", "content": f"Hello! Could you use these ideas {ideas_4_subset} and come up with 3 distinct key themes or related keywords for every idea there. Please return them in a json with a description for each, there should be {len(ideas_4_subset)*3} ideas in the final ouptput with a keyword and description. Thank you."}],response_format = { "type": "json_object" })
+#   print(completion4.choices[0].message.content)
+#   output += [completion4.choices[0].message.content] 
+
+
+# df_combined_5 = pd.DataFrame()
+# for i in [3]:
+#   temp=json.loads(output[i])
+#   for col in pd.json_normalize(temp, sep='_'):
+#     print(col)
+#     df_temp = pd.json_normalize(temp, col).assign(Category=col)
+#     df_combined_5 = pd.concat([df_combined_5, df_temp], ignore_index=True)
+#   print(df_combined_5)
+
+# #df_combined_4.to_csv('themes4.csv', index=False)
+
+# #print(df_combined_4)
+
+# output.to_csv('round4_rawoutput.csv', index=False)
+
+# temporary.to_csv('round4_first180.csv', index=False)
+
+# pd.concat([data3, df_combined_2, df_combined_3]).reset_index().drop(['index'], axis = 1)
+
+
+#final_df.to_csv('final2rounds.csv', index=False)
+
+final_df = pd.read_csv('final2rounds.csv')
+
+
+append_source = ['whitespace generated' for x in range(len(final_df))]
+append_maturity = ['whitespace' for x in range(len(final_df))]
+
+append_df = pd.DataFrame({'Prompt': final_df.keyword.values, 'Source': append_source, 'Maturity': append_maturity, 'Description': final_df.description.values})
 
 full_data = pd.concat([data, append_df]).reset_index().drop(['index'], axis = 1)
 
-full_data.iloc[15].Prompt = full_data.iloc[15].Prompt.split('\r')[0]
-
 full_data.to_csv('data.csv', index=False)
-
-
-data_2 = pd.read_csv('data.csv')
-
-
 
