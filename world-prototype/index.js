98b83572600fada4bded3a0f0578b5376adf16d0
Annie Miller <anniemiller@berkeley.edu>
2024-08-09 15:00:17 -0400

    adding world prototype subset

--- world-prototype/index.js
+++ world-prototype/index.js
@@ -1,10 +1,10 @@
 import { fetchSheet,abstract,convert } from "../src/index.js";
 const id = '1YTIa2svFCJjWG0SdZN9OtAsdoK6LdrmClrF8hXVPs9Y'
-const tab = 'Green Aviation only'
-const path = './data-green.csv'
+const tab = 'World Prototype Data'
+const path = './data-world.csv'
 
 await fetchSheet(id,tab,path)
 
-await convert('./data-green.csv','./output.json')
+await convert('./data-world.csv','./output.json')
 
 await abstract('./output.json')
